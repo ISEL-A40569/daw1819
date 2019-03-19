4ec7bf0d88a0d6e98c0c6a35726dafdc60cb93da
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2019-03-19 22:32:37 +0000

    Basic Auth initial implementation

--- src/main/java/pt/ipl/isel/leic/daw/project/controller/ProjectController.java
+++ src/main/java/pt/ipl/isel/leic/daw/project/controller/ProjectController.java
@@ -3,6 +3,7 @@ package pt.ipl.isel.leic.daw.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pt.ipl.isel.leic.daw.project.exception.ResourceNotFoundException;
@@ -19,7 +20,7 @@ public class ProjectController {
 
     @GetMapping("/api/project/{id}")
     public Project getProject(@PathVariable long id) {
-        return projectRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Project does not exist!"));
+        return projectRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Project does not exist!")); // TODO: build error messages
     }
 
     @GetMapping("/api/projects")
@@ -28,8 +29,9 @@ public class ProjectController {
     }
 
     @PostMapping("/api/project")
-    public Project postProject(@Valid @RequestBody Project project) {
-        return  projectRepository.save(project);
+    public ResponseEntity<Project> postProject(@Valid @RequestBody Project project) {
+        return ResponseEntity.status(201)
+                .body(projectRepository.save(project));     //  TODO: build response messages properly (with siren)
     }
 
     @PutMapping("/api/project/{id}")

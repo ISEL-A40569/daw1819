4da41670c6e88abad8a0ed8d88680d154b5f97c5
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2019-03-23 11:41:42 +0000

    Basic Auth working

---
+++ src/main/java/pt/ipl/isel/leic/daw/project/security/BasicAuthenticationPoint.java
@@ -0,0 +1,26 @@
+package pt.ipl.isel.leic.daw.project.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class BasicAuthenticationPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("dawRealm");
+        super.afterPropertiesSet();
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setStatus(401);
+    }
+}
--- src/main/java/pt/ipl/isel/leic/daw/project/security/SecurityConfig.java
+++ src/main/java/pt/ipl/isel/leic/daw/project/security/SecurityConfig.java
@@ -1,31 +1,34 @@
-//package pt.ipl.isel.leic.daw.project.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//import java.util.Base64;
-//
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests().anyRequest().authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//
-//
-//    @Autowired
-//    public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
-//            throws Exception
-//    {
-//        auth.inMemoryAuthentication()
-//                .withUser("user1")
-//                .password(Base64.getEncoder().encode("123456".getBytes()).toString())
-//                .roles("USER");
-//    }
-//}
+package pt.ipl.isel.leic.daw.project.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private BasicAuthenticationPoint basicAuthenticationEntryPoint;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic().authenticationEntryPoint(basicAuthenticationEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("USER");
+    }
+}
